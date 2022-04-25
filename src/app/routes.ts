cbcbdadee623486b4f821a21b87398ac39417963
Magali Dugué <77433765+magalidugue@users.noreply.github.com>
2022-04-25 09:07:38 +0200

    page de confirmation

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,3 +1,4 @@
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 //import { SessionRouteActivator } from './sessionsRouteActivator.service';
 import { BookingFormComponent } from './bookingForm.component';
 import { Routes } from "@angular/router";
@@ -8,7 +9,9 @@ import { VisitRouteActivator } from "./visitsRouteActivator.service";
 export const appRoutes: Routes = [
   {path: '', redirectTo: 'visites', pathMatch: 'full'},
   { path: 'reservation', component: BookingFormComponent },
+  {path: 'confirmation', component: ConfirmationComponent},
   { path: 'visites', component: VisitListComponent },
   { path: 'visites/:id', component: VisitDetailsComponent, canActivate: [VisitRouteActivator] },
+
 
 ]
